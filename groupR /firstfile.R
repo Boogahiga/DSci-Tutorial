09efa1321eb18f869ad46dc42af4e5012cd8c03c
Boogahiga <huang.tzu.han26@gmiail.com>
2016-10-18 13:06:01 +0800

    .

--- groupR /firstfile.R
+++ groupR /firstfile.R
@@ -1,18 +1,70 @@
-library(httr)
+######################
+##       Setup      ##
+######################
+
+## Install packages ??
+# install.packages("XML")
+# install.packages("RCurl")
+# install.packages("httr")
+
+
+
+
+
+## Setup & Clear memory (variables) 
+rm(list=ls(all.names=TRUE))
 library(XML)
 library(RCurl)
+library(httr)
 
-# get download page url path 
-pathURL = "https://www.ptt.cc/bbs/movie/index.html"
-tempDATA = getURL(pathURL)
+## Check language 
+# Sys.getlocale("LC_ALL")
+Sys.setlocale(category = "LC_ALL", locale = "cht")
+startNo = 4637
+endNo   = 4641 
+subPath <- "https://www.ptt.cc/bbs/movie/index"
+alldata = data.frame()
 
-# /n, /t: html => xxl
-xmldoc = htmlParse(tempDATA, encoding = "UTF-8")
+for( pid in startNo:endNo )
+    {
+      urlPath <- paste(subPath, pid, ".html", sep='')
+      temp    <- getURL(urlPath, encoding = "big5")
+      xmldoc  <- htmlParse(temp)
+      
+      ## xpathSApply vs. xpathApply
+      ## the backscape symbol \"
+      title   <- xpathSApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
+      
+      ## Clean data "\n\t..."
+      # regular expression: http://regexr.com/ 
+      # title = gsub("\\[n-t]", "", title) //not working?? 
+      title   <- gsub("\n", "", title)
+      title   <- gsub("\t", "", title)
+      
+      
+      # no value for path 
+      path    <- xpathSApply(xmldoc, "//div[@class='title']/a//@href")
+      Erroresult<- tryCatch({
+        subdata <- data.frame(title, path)
+        alldata <- rbind(alldata, subdata)
+      }, warning = function(war) {
+        print(paste("MY_WARNING:  ", urlPath))
+      }, error = function(err) {
+        print(paste("MY_ERROR:  ", urlPath))
+      }, finally = {
+        print(paste("End Try&Catch", urlPath))
+      })
+    }
 
-# \
-title = xpathApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
-url = xpathApply(xmldoc, "//div[@class=\"title\"]/a//@href",  xmlValue)
-date = xpathApply(xmldoc, "//div[@class=\"date\"]", xmlValue)
-alldata = data.frame(title)
-
-
+write.table(alldata, file = "movie.csv")
+suburlPath <- "https://www.ptt.cc"
+for( i in 1:length(alldata[,1]) )
+    {
+      ipath   <- paste(suburlPath, alldata$path[i], sep='')
+      print(ipath)
+      content <- getURL(ipath, encoding = "big5")
+      xmldoc  <- htmlParse(content)
+      article <- xpathSApply(xmldoc, "//div[@id=\"main-content\"]", xmlValue)
+      filename<- paste("./data/", i, ".csv", sep='')
+      write.csv(article, filename)
+    }
